2bb83c5ed150c03313d97a70496d37bdcd835786
Tomoyuki Sakurai <y@trombik.org>
2019-06-01 11:51:42 +0900

    bugfix: use https instead of http

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -18,7 +18,7 @@ end
 
 describe command "pkg -vv" do
   its(:stdout) { should match(/FreeBSD_latest:/) }
-  its(:stdout) { should match(%r{url\s+:\s+"pkg\+http://pkg.FreeBSD.org/FreeBSD:\d+:amd64/latest"}) }
+  its(:stdout) { should match(%r{url\s+:\s+"pkg\+https://pkg.FreeBSD.org/FreeBSD:\d+:amd64/latest"}) }
   its(:stdout) { should match(/enabled\s+:\s+yes/) }
   its(:stdout) { should match(/priority\s+:\s+100/) }
   its(:stdout) { should match(/mirror_type\s+:\s+"SRV"/) }
